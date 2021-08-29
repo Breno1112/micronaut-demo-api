b2ce60701553c1a3e3442384b5d88c3ad309097b
Breno Fachini <dev.brenofachini@gmail.com>
2021-08-29 00:06:38 -0300

    this should have added data to databse. Need to fix it later on

--- api/src/test/java/com/breno/api/controllers/UserControllerTest.java
+++ api/src/test/java/com/breno/api/controllers/UserControllerTest.java
@@ -2,14 +2,23 @@ package com.breno.api.controllers;
 
 import com.breno.api.domain.dtos.UserDTO;
 import com.breno.api.engines.UserEngine;
+import io.micronaut.core.io.ResourceLoader;
 import io.micronaut.http.client.HttpClient;
 import io.micronaut.http.client.annotation.Client;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import jakarta.inject.Inject;
+import org.h2.tools.RunScript;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.sql.SQLException;
 import java.util.List;
+import java.util.Optional;
 
 
 @MicronautTest
@@ -22,11 +31,28 @@ public class UserControllerTest {
     @Client("/")
     HttpClient client;
 
+    @Inject
+    DataSource dataSource;
+
+    @Inject
+    ResourceLoader resourceLoader;
+
     @Test
     void testItReturnsEmptyUserList(){
         final UserDTO[] response = client.toBlocking().retrieve("/users", UserDTO[].class);
         Assertions.assertEquals(0, response.length);
     }
+
+    @Test
+    void testItReturnsSingleUser() throws SQLException {
+        final Optional<InputStream> optionalSqlScript = this.resourceLoader.getResourceAsStream("addSingleUser.sql");
+        final InputStream sqlScript = optionalSqlScript.get();
+        RunScript.execute(dataSource.getConnection(), new InputStreamReader(sqlScript, StandardCharsets.UTF_8));
+        final UserDTO[] response = client.toBlocking().retrieve("/users", UserDTO[].class);
+        Assertions.assertEquals(0, response.length);
+    }
+
+
 
 
 }
